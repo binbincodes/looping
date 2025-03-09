3928d67b28f24b03e976bfa68dbd74f3b19e07b1
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2025-03-09 17:32:18 +0700

    add for off and for in

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ for (let z = 1; z <= 5; z++) {
 let x = 1
 while(x <= 5) {
         console.log('data x ke -', x)
-    X++
+    x++
 }
 
 let y = 1
@@ -19,3 +19,22 @@ do {
     console.log('data y ke -', y)
     y++
 } while (y <= 10)
+
+/*
+! PERULANGAN DI JAVASCRIPT
+? FOR, FOR OF, FOR IN, FOREACH
+* WHILE & DO WHILE 
+! MAP
+**/
+
+const pacarSaya = ['rabin','yuri', 'kefier','andreas','Ci-N']
+// for of supaya tau isian index atau dalam pacar saya itu apa atau mendapatkan value nya langsung
+
+for(let pacar of pacarSaya) {
+    console.log(pacar)
+}
+
+// for in = index dapat index nya
+for(let pacar in pacarSaya) {
+    console.log(pacar)
+}
